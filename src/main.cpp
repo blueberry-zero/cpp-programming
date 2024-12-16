69ac405240c3dd20b56ab34c6a8eda892c2097a7
blueberry-zero <blueberry-zero@users.noreply.github.com>
2024-12-15 22:38:17 -0500

    feat: simple cost calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello World!" << "\n";
-    std::cout << "I am learning C++!" << "\n";
-    // comment
+  std::string greeting = "Hello world!";
+  std::cout << greeting << "\n";
 
-    /*
-     * block comment
-     */
+  // Create variables of different data types
+  int items = 50;
+  double cost_per_item = 9.99;
+  double total_cost = items * cost_per_item;
+  char currency = '$';
 
-    int my_num = 12;
-    std::cout << my_num << "\n";
+  // Print variables
+  std::cout << "Number of items: " << items << "\n";
+  std::cout << "Cost per item: " << cost_per_item << "" << currency << "\n";
+  std::cout << "Total cost = " << total_cost << "" << currency << "\n";
 
-    my_num = 15;
-    std::cout << my_num << "\n";
-
-    double myDub = 15.1f;
-    std::cout << myDub << "\n";
-
-    char myChar = 'a';
-    std::cout << myChar << "\n";
-
-    std::cout << "Give me a num!" << "\n";
-    std::cin >> my_num;
-    std::cout << "Your num was " << my_num << "\n";
-
-    char a = 65;
-    char b = 66;
-    char c = 67;
-
-    std::cout << a << b << c << "\n";
-
-    return 0;
+  return 0;
 }
